5892eee6bdaf22f896081f7393c3877fb744b490
Johnny <jonathanbc2012@gmail.com>
2024-11-08 18:55:55 +0100

    feat: hero section created

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,31 @@
-
+import { logo } from "../assets";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full pt-4 mb-10">
+        <img src={logo} alt="logo" className="w-28 object-contain" />
+        <button
+          type="button"
+          onClick={() => {
+            window.open("https://github.com/JohnnyBC2022/resume-articulos-ai");
+          }}
+          className="black_btn"
+        >
+          Github
+        </button>
+      </nav>
 
-export default Hero
+      <h1 className="head_text">
+        Resume artículos con <br className="max-md:hidden" />{" "}
+        <span className="blue_gradient">Open AI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Ahorra tiempo de lectura con ResAI, una aplicación para resumir artículos de código abierto que transforma artículos extensos en resúmenes claros y
+        concisos.
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
